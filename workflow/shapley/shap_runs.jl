ff38361964a73e1afa7c5fd609f2ebce92ac253e
parinbhaduri <pbb62@cornell.edu>
2026-01-28 11:58:35 -0500

    updated shap_runs.jl

--- workflow/shapley/shap_runs.jl
+++ workflow/shapley/shap_runs.jl
@@ -65,7 +65,7 @@ events = combine(groupby(haz_cat, "category")) do group
     )
 end
 =#
-flood_years = [1982,2010,2013,1988] #vcat(events.year_min,events.year_med, events.year_max)
+flood_years = [2013] #vcat(events.year_min,events.year_med, events.year_max)
 one_shock = true
 repeat_shocks = false
 
@@ -149,7 +149,7 @@ for flood_shock in flood_years
     filename = joinpath(data_dir,"$(flood_shock)_abm_data.h5")
     n_years = 20
     n_agents = 755
-    
+    #=
     h5open(filename, "w") do file
         # Create datasets with chunking for efficient I/O
         chunk_size = (1, n_agents, n_years+1, 1)
@@ -169,8 +169,9 @@ for flood_shock in flood_years
         write(file, "n_agents", n_agents)
         write(file, "GEOID", unique(phil_bg.GEOID)) 
         write(file, "n_years", n_years)
-        write(file, "price_vars", string.(shap_adata[9:end]))    
+        write(file, "price_vars", string.(shap_adata[9:end]))   
     end
+    =#
     #=
     # Set up pop shares during shock data file 
     pop_share_file = joinpath(data_dir,"$(flood_shock)_pop_share_data.h5")
@@ -200,7 +201,7 @@ for flood_shock in flood_years
 
     log_info("Processing $n_combs parameter combinations in $n_chunks chunks")
 
-    for chunk_idx in 1:n_chunks
+    for chunk_idx in 3:n_chunks
         
         # Get subset of combinations for this chunk
         start_idx = (chunk_idx - 1) * chunk_size + 1
