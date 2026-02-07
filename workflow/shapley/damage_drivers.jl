7073db129a3b32efb50d2ff36f2b7ec137e03865
parinbhaduri <pbb62@cornell.edu>
2026-02-06 22:21:16 -0500

    Updated shap analysis for loss burdens

--- workflow/shapley/damage_drivers.jl
+++ workflow/shapley/damage_drivers.jl
@@ -3,8 +3,8 @@ import Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
-using Distributed
-addprocs(4, exeflags="--project=$(Base.active_project())")
+#using Distributed
+#addprocs(4, exeflags="--project=$(Base.active_project())")
 using Distributed, SlurmClusterManager
 
 addprocs(SlurmManager())
@@ -27,6 +27,9 @@ end
     using Agents
     using CHANCE_C
     using StatsBase
+    using Parquet2
+    using ExperimentalDesign
+    using Distributions
 end
 
 include(joinpath(dirname(@__DIR__),"src","functions.jl"))
@@ -37,59 +40,32 @@ EvoTreeRegressor = @load EvoTreeRegressor pkg=EvoTrees
 
 out_dir = joinpath(@__DIR__,"data","shap_runs")
 
+phil_damages = DataFrame(CSV.File(joinpath(dirname(pwd()), "philadelphia-data","flood_hazard", "data","phil_flood_dmg_ens.csv")))
+phil_exp = DataFrame(CSV.File(joinpath(dirname(pwd()), "philadelphia-data","model_inputs", "phil_flood_hist_year.csv")))
+
 ##Define outcome and hazard variables
-outcome = "flood_loss"
 haz_size = "High"
-agent_cat = "high"
+agent_cats = ["low","high","med"]
+event_years = [1989,1996,2011] #High: [1989,1996,2011], Medium: [1981,1991,2018], Low: [1988,2010,2013]
+#Select a random subsample of features
+DOE = DesignDistribution((year = CategoricalFactor(event_years), model = DiscreteUniform(1, 159500), DDF = DiscreteUniform(1, 500)))
+factor_samples = rand(DOE, 159500*3)
+factor_samples.matrix.year = Float64.(factor_samples.matrix.year)
 
-# Load simulated results for each event 
-filtered_files = filter(file -> occursin(Regex("burden_$(agent_cat)\\.csv\$"),file), readdir(joinpath(out_dir,"post_process",outcome,haz_size)))
-println("Loading feature array for each event...")
-all_dfs = [CSV.read(joinpath(joinpath(out_dir,"post_process",outcome,haz_size), file), DataFrame, 
-                    select=[:model, :DDF]) 
-           for file in filtered_files]
-
-features = vcat(all_dfs...)
-#Select a subset of features
-sub_ind = sample(1:Int(size(features,1)/2), 1000, replace=false)
-features_explain = vcat([df[sub_ind, :] for df in all_dfs]...)
-
-targets = vcat([CSV.read(joinpath(joinpath(out_dir,"post_process",outcome,haz_size), file), DataFrame, 
-                    select=[:value]) for file in filtered_files]...
-)[!,1]
-
-haz_cat = DataFrame(CSV.File(joinpath(dirname(pwd()), "philadelphia-data","model_inputs", "phil_flood_hist_categories.csv")))
-fld_extents = zeros(size(features,1))
-fld_extents_explain = zeros(size(features_explain,1))
-
-println("Storing flood extents for each event...")
-# Record total flood extent within exposed area for each year
-for (event_idx,year) in enumerate([1989,2011]) #1996,
-    fld_extent = haz_cat[haz_cat.year .== year, :total_extents]
-    fld_extents[((event_idx - 1) * Int(size(features,1)/2) + 1):(event_idx * Int(size(features,1)/2))] .= fld_extent[1]
-    fld_extents_explain[((event_idx - 1) * Int(size(features_explain,1)/2) + 1):(event_idx * Int(size(features_explain,1)/2))] .= fld_extent[1]
-end
-
-features.fld_extents = fld_extents
-features_explain.fld_extents = fld_extents_explain
-
-#targets = copy(vcat(out_df[:,1:21],out_df_2))#[idx, :]
-#targets = vcat([CSV.read(joinpath(out_dir,"post_process",outcome,haz_size,file), DataFrame) for file in filtered_files]...)
 # define function for parallelized Shapley calculation
 @everywhere function predict_var(model, data)
-    pred = DataFrame(pop_pred = MLJ.predict(model,data))
-    return pred
+    return DataFrame(pop_pred = MLJ.predict(model,data))
 end
 
 # make regression trees and compute Shapley values
-function shapley_reg(features, target, explain)
-    shap_df = DataFrame(feature_name = names(features))
+function shapley_reg(features, target)
+    #shap_df = DataFrame(feature_name = names(features))
     println("Fitting Tree...")
     out_reg_tree = EvoTreeRegressor(nrounds=200, max_depth=5);
     out_reg_mach = machine(out_reg_tree, features, target);
     MLJ.fit!(out_reg_mach, force=true)
     
-    #explain = copy(features)
+    explain = copy(features)
     reference = copy(features)
     println("Calculating Shapley Indices...")
     shap_out = ShapML.shap(explain = explain, 
@@ -99,15 +75,45 @@ function shapley_reg(features, target, explain)
                             sample_size = 50, #100
                             parallel = :samples, 
                             seed = 1)
-    println("Saving Data for Year...")
+    println("Saving Data...")
     shap_grouped = groupby(shap_out, :feature_name)
     shap_summary = combine(shap_grouped, 
         :shap_effect => (x -> mean(abs.(x))))
-    rename!(shap_summary, Dict(:shap_effect_function => Symbol("mean_$(yr)")))
-    shap_df = innerjoin(shap_df, shap_summary, on=:feature_name)
-    return shap_df
+    rename!(shap_summary, Dict(:shap_effect_function => Symbol("mean_shap")))
+    #shap_df = innerjoin(shap_df, shap_summary, on=:feature_name)
+    return shap_summary
 end
-println("Starting Shapley Index calculation...")
 
-shap_df = shapley_reg(features, targets, features_explain)
-CSV.write(joinpath(out_dir, "post_process","shapley_indices","$(haz_size)_fld_shap_indices_flpn_burden_$(agent_cat)_sub.csv"), shap_df)
+
+
+for agent_cat in agent_cats
+    # Load simulated results for each event 
+    println("Starting shap analysis for $(agent_cat) income group...")
+    println("Loading feature array for each event...")
+    ds = Parquet2.Dataset(joinpath(out_dir, "post_process","flood_loss",haz_size))
+    filtered_files = findall(file -> occursin(Regex("loss_$(agent_cat)"),file), string.(Parquet2.filelist(ds)))
+    all_dfs = mapreduce(vcat, enumerate(filtered_files)) do (i,f)
+        append!(ds, f)
+        df = ds[i] |> Parquet2.select(:model, :DDF, :burden_value) |> DataFrame
+        #Subset to sampled features
+        m = match(r"(\d{4})", string.(Parquet2.filelist(ds))[f])
+        fl_year = parse(Int,m.match)
+        event_samples = factor_samples.matrix[factor_samples.matrix.year .== Float64(fl_year),:]
+        event_df = innerjoin(t_df, event_samples, on = [:model, :DDF])
+
+        # Record total flood extent within exposed area for each year
+        dmg_bgs = unique(phil_damages[phil_damages[!,"naccs_loss_$(fl_year)"] .> 0.0, :bg_id])
+        event_extent = sum(phil_exp[(phil_exp.year .== fl_year) .& (phil_exp.GEOID .∈ Ref(dmg_bgs)), :flood_extent])
+        replace!(event_df.year, fl_year => event_extent)
+        return event_df
+    end
+
+    features = select(all_dfs, Not(:burden_value))
+    targets = select(all_dfs, :burden_value)[!,1]
+
+    println("Starting Shapley Index calculation...")
+
+    shap_df = shapley_reg(features, targets)
+    CSV.write(joinpath(out_dir, "post_process","shapley_indices","$(haz_size)_fld_shap_indices_flpn_burden_$(agent_cat).csv"), shap_df)
+    GC.gc()
+end
